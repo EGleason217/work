497a16e548d7af9551cd535dbb5063cbe3a54410
EGleason217 <erik@gleasoncf.com>
2020-10-05 08:29:52 -0400

    first commit

--- pdfcitizen/pdfcitizen/settings.py
+++ pdfcitizen/pdfcitizen/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$8zqo%#sx8ub^fp()v%dpts&f=ne!%zbme@(ydj4_&leldgvyl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
